59dc6f3d15d4f6b5165eb721eaf7fb9391fc65bf
Sema <sema@pyva.net>
2018-10-14 23:58:13 -0400

    Headless MIDI player

--- midi/MidiPlayer/main.js
+++ midi/MidiPlayer/main.js
@@ -7,9 +7,25 @@ window.MidiPlayer = class MidiPlayer extends WebAudioPluginCompositeNode {
 
   createNodes() {
     var self = this;
+    var player;
     this.inputsMidi[0] = JZZ.Widget();
     this.outputsMidi[0] = this.inputsMidi[0];
+    this.load = function(data) {
+      if (player) {
+        player.stop();
+        player.disconnect(this.outputsMidi[0]);
+      }
+      var smf = new JZZ.MIDI.SMF(data);
+      player = smf.player();
+      player.connect(this.outputsMidi[0]);
+    };
+    this.loop = function(x) { if (player) player.loop(x); }
+    this.play = function() { if (player) player.play(); }
+    this.pause = function() { if (player) player.pause(); }
+    this.resume = function() { if (player) player.resume(); }
+    this.stop = function() { if (player) player.stop(); }
   };
+
   connectNodes() {
   };
 
