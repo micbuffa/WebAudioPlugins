e3990277c010be69286da1af0270a27599c77130
Sema <sema@pyva.net>
2019-01-21 16:04:26 -0500

    MIDI Player can open files

--- midi/JZZ/JZZ.gui.Player.js
+++ midi/JZZ/JZZ.gui.Player.js
@@ -3,7 +3,7 @@
     module.exports = factory;
   }
   else if (typeof define === 'function' && define.amd) {
-    define('JZZ.midi.SMF', ['JZZ', 'JZZ.midi.SMF'], factory);
+    define('JZZ.gui.Player', ['JZZ', 'JZZ.midi.SMF'], factory);
   }
   else {
     factory(JZZ);
@@ -12,6 +12,9 @@
 
   if (!JZZ.gui) JZZ.gui = {};
   if (JZZ.gui.Player) return;
+
+  function empty() {}
+  var _noBtn = { on: empty, off: empty, disable: empty, div: {} };
 
   function Btn(html) {
     this.div = document.createElement('div');
@@ -48,8 +51,11 @@
   var svg_stop = '<svg fill="#555" height="18" viewBox="0 0 24 24" width="18" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h24v24H0z" fill="none"/><path d="M6 6h12v12H6z"/></svg>';
   var svg_loop = '<svg fill="#555" height="18" viewBox="0 0 24 24" width="18" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 5V1L7 6l5 5V7c3.31 0 6 2.69 6 6s-2.69 6-6 6-6-2.69-6-6H4c0 4.42 3.58 8 8 8s8-3.58 8-8-3.58-8-8-8z"/></svg>';
   var svg_more = '<svg fill="#555" height="18" viewBox="0 0 24 24" width="18" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 3v9.28c-.47-.17-.97-.28-1.5-.28C8.01 12 6 14.01 6 16.5S8.01 21 10.5 21c2.31 0 4.2-1.75 4.45-4H15V6h4V3h-7z"/></svg>';
+  var svg_open = '<svg fill="#555" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0V0z"/><path d="M10 4H2v16h20V6H12l-2-2z"/></svg>';
 
-  function _createGUI(self) {
+  function _stopProp(e) { e.stopPropagation(); e.preventDefault(); }
+
+  function _createGUI(self, arg) {
     self.gui = document.createElement('div');
     self.gui.style.display = 'inline-block';
     self.gui.style.position = 'relative';
@@ -60,55 +66,106 @@
     self.gui.style.width = '270px';
     self.gui.style.height = '40px';
 
-    self.playBtn = new Btn(svg_play);
-    self.playBtn.div.style.left = '8px';
-    self.playBtn.div.title = 'play';
-    self.playBtn.div.addEventListener('click', function() { self.play(); });
-    self.gui.appendChild(self.playBtn.div);
+    var left = 8;
+    var right = 238;
+    var step = 28;
 
-    self.pauseBtn = new Btn(svg_pause);
-    self.pauseBtn.div.style.left = '36px';
-    self.pauseBtn.div.title = 'pause';
-    self.pauseBtn.div.addEventListener('click', function() { self.pause(); });
-    self.gui.appendChild(self.pauseBtn.div);
+    if (arg.play) {
+      self.playBtn = new Btn(svg_play);
+      self.playBtn.div.style.left = left + 'px';
+      left += step;
+      self.playBtn.div.title = 'play';
+      self.playBtn.div.addEventListener('click', function() { self.play(); });
+      self.gui.appendChild(self.playBtn.div);
+    }
+    else self.playBtn = _noBtn;
 
-    self.stopBtn = new Btn(svg_stop);
-    self.stopBtn.div.style.left = '64px';
-    self.stopBtn.div.title = 'stop';
-    self.stopBtn.div.addEventListener('click', function() { self.stop(); });
-    self.gui.appendChild(self.stopBtn.div);
+    if (arg.pause) {
+      self.pauseBtn = new Btn(svg_pause);
+      self.pauseBtn.div.style.left = left + 'px';
+      left += step;
+      self.pauseBtn.div.title = 'pause';
+      self.pauseBtn.div.addEventListener('click', function() { self.pause(); });
+      self.gui.appendChild(self.pauseBtn.div);
+    }
+    else self.pauseBtn = _noBtn;
 
-    self.loopBtn = new Btn(svg_loop);
-    self.loopBtn.div.style.left = '92px';
-    self.loopBtn.div.title = 'loop';
-    self.loopBtn.div.addEventListener('click', function() { self.loop(); });
-    self.gui.appendChild(self.loopBtn.div);
+    if (arg.stop) {
+      self.stopBtn = new Btn(svg_stop);
+      self.stopBtn.div.style.left = left + 'px';
+      left += step;
+      self.stopBtn.div.title = 'stop';
+      self.stopBtn.div.addEventListener('click', function() { self.stop(); });
+      self.gui.appendChild(self.stopBtn.div);
+    }
+    else self.stopBtn = _noBtn;
 
-    self.moreBtn = new Btn(svg_more);
-    self.moreBtn.div.style.left = '238px';
-    self.moreBtn.div.title = 'midi';
-    self.moreBtn.div.addEventListener('click', function() { self.settings(); });
-    self.gui.appendChild(self.moreBtn.div);
+    if (arg.loop) {
+      self.loopBtn = new Btn(svg_loop);
+      self.loopBtn.div.style.left = left + 'px';
+      left += step;
+      self.loopBtn.div.title = 'loop';
+      self.loopBtn.div.addEventListener('click', function() { self.loop(); });
+      self.gui.appendChild(self.loopBtn.div);
+    }
+    else self.loopBtn = _noBtn;
 
-    self.select = document.createElement('select');
-    self.select.style.position = 'absolute';
-    self.select.style.top = '30px';
-    self.select.style.left = '40px';
-    self.select.style.width = '230px';
-    self.select.style.display = 'none';
-    self.select.style.zIndex = 1;
-    self.select.addEventListener('click', function() { self._selected(); });
-    self.select.addEventListener('keydown', function(e) { self._keydown(e); });
-    self.select.addEventListener('focusout', function() { self._closeselect(); });
+    if (arg.more) {
+      self.moreBtn = new Btn(svg_more);
+      self.moreBtn.div.style.left = right + 'px';
+      right -= step;
+      self.moreBtn.div.title = 'midi';
+      self.moreBtn.div.addEventListener('click', function() { self.settings(); });
+      self.gui.appendChild(self.moreBtn.div);
 
-    self.gui.appendChild(self.select);
+      self.select = document.createElement('select');
+      self.select.style.position = 'absolute';
+      self.select.style.top = '30px';
+      self.select.style.left = '40px';
+      self.select.style.width = '230px';
+      self.select.style.display = 'none';
+      self.select.style.zIndex = 1;
+      self.select.addEventListener('click', function() { self._selected(); });
+      self.select.addEventListener('keydown', function(e) { self._keydown(e); });
+      self.select.addEventListener('focusout', function() { self._closeselect(); });
+
+      self.gui.appendChild(self.select);
+    }
+    else self.moreBtn = _noBtn;
+
+    if (arg.open) {
+      self.openBtn = new Btn(svg_open);
+      self.openBtn.div.style.left = right + 'px';
+      right -= step;
+      self.openBtn.div.title = 'file';
+      self.gui.appendChild(self.openBtn.div);
+
+      self.fileInput = document.createElement('input');
+      self.fileInput.type = 'file';
+      self.fileInput.style.position = 'absolute';
+      self.fileInput.style.top = '-1000px';
+      self.fileInput.accept = '.mid, .midi, .kar, .rmi';
+      self.gui.appendChild(self.fileInput);
+
+      if (window.FileReader) {
+        self.openBtn.off();
+        self.openBtn.div.addEventListener('click', function() { self.fileInput.click(); });
+        self.fileInput.addEventListener('change', function(e) { _stopProp(e); self.readFile(e.target.files[0]); });
+        self.gui.addEventListener('drop', function(e) { _stopProp(e); self.openBtn.off(); self.readFile(e.dataTransfer.files[0]); });
+        self.gui.addEventListener('dragover', function(e) { _stopProp(e); self.openBtn.on(); e.dataTransfer.dropEffect = 'copy'; });
+        self.gui.addEventListener('dragexit', function(e) { _stopProp(e); self.openBtn.off(); });
+      }
+    }
+    else self.openBtn = _noBtn;
+
+    self.rlen = right - left + 10;
 
     self.rail = document.createElement('div');
     self.rail.style.display = 'inline-block';
     self.rail.style.position = 'absolute';
     self.rail.style.top = '19px';
-    self.rail.style.left = '125px';
-    self.rail.style.width = '100px';
+    self.rail.style.left = (left + 5) + 'px';
+    self.rail.style.width = self.rlen + 'px';
     self.rail.style.height = '0';
     self.rail.style.padding = '1px';
     self.rail.style.borderStyle = 'solid';
@@ -141,27 +198,46 @@
   var _floating = 0;
   function Player(x, y) {
     if (!(this instanceof Player)) return new Player(x, y);
-    _createGUI(this);
-    if (typeof x == 'string') {
+    var arg = {
+      at: undefined,
+      x: undefined,
+      y: undefined,
+      play: true,
+      record: false,
+      pause: true,
+      stop: true,
+      loop: true,
+      open: false,
+      more: true,
+      close: false
+    };
+    for (var k in arg) if (arg.hasOwnProperty(k) && typeof x[k] != 'undefined') arg[k] = x[k];
+    if (typeof arg.at == 'undefined') arg.at = x;
+    if (typeof arg.x == 'undefined') arg.x = x;
+    if (typeof arg.y == 'undefined') arg.y = y;
+    _createGUI(this, arg);
+
+    if (typeof arg.at == 'string') {
       try {
-        document.getElementById(x).appendChild(this.gui);
+        document.getElementById(arg.at).appendChild(this.gui);
         return this;
       }
       catch(e) {}
     }
     try {
-      x.appendChild(this.gui);
+      arg.at.appendChild(this.gui);
       return this;
     }
     catch(e) {}
-    if (x != parseInt(x) || y != parseInt(y)) {
-      x = _floating * 45 + 5;
-      y = _floating * 15 + 5;
+
+    if (arg.x != parseInt(arg.x) || arg.y != parseInt(arg.y)) {
+      arg.x = _floating * 45 + 5;
+      arg.y = _floating * 15 + 5;
       _floating++;
     }
     this.gui.style.position = 'fixed';
-    this.gui.style.top = x + 'px';
-    this.gui.style.left = y + 'px';
+    this.gui.style.top = arg.x + 'px';
+    this.gui.style.left = arg.y + 'px';
     this.gui.style.opacity = 0.9;
     var self = this;
     this.gui.addEventListener('mousedown', function(e) { self._startmove(e); });
@@ -173,6 +249,7 @@
     this.stopBtn.disable();
     this.loopBtn.disable();
     this.moreBtn.disable();
+    this.openBtn.off();
     this.rail.style.borderColor = '#aaa';
     this.rail.style.backgroundColor = '#888';
     this.caret.style.borderColor = '#aaa';
@@ -195,7 +272,9 @@
     this._player.onEnd = function() { self._onEnd(); };
     this.enable();
   };
+  Player.prototype.onEnd = function() {};
   Player.prototype._onEnd = function() {
+    this.onEnd();
     if (this._loop && this._loop != -1) this._loop--;
     if (!this._loop) {
       if (this._moving) clearInterval(this._moving);
@@ -213,7 +292,7 @@
     }
   };
   Player.prototype._move = function() {
-    var off = Math.round(this._player.position() * 100 / this._player.duration()) - 5;
+    var off = Math.round(this._player.position() * this.rlen / this._player.duration()) - 5;
     this.caret.style.left = off + 'px';
   };
   Player.prototype.play = function() {
@@ -296,6 +375,24 @@
         this.loopBtn.div.title = 'loop';
       }
     }
+  };
+
+  Player.prototype.readFile = function(f) {
+    var self = this;
+    var reader = new FileReader();
+    reader.onload = function(e) {
+      var data = '';
+      var bytes = new Uint8Array(e.target.result);
+      for (var i = 0; i < bytes.length; i++) data += String.fromCharCode(bytes[i]);
+      try {
+        var smf = new JZZ.MIDI.SMF(data);
+        self.stop();
+        self.load(smf);
+        self.play();
+      }
+      catch (err) {}
+    };
+    reader.readAsArrayBuffer(f);
   };
 
   // selecting MIDI
@@ -413,14 +510,16 @@
     }
   };
   Player.prototype._mousemove = function(e) {
-    e.preventDefault();
     if (this._player && typeof this._caretX != 'undefined') {
+      e.preventDefault();
       var to = this._caretPos + e.clientX - this._caretX;
       if (to < 0) to = 0;
-      if (to > 100) to = 100;
-      this.jump(this.duration() * to / 100.0);
+      //if (to > 100) to = 100;
+      if (to > this.rlen) to = this.rlen;
+      this.jump(this.duration() * to * 1.0 / this.rlen);
     }
     else if (typeof this._startX != 'undefined') {
+      e.preventDefault();
       this.gui.style.left = this._startX - this._clickX + e.clientX + 'px';
       this.gui.style.top = this._startY - this._clickY + e.clientY + 'px';
     }
